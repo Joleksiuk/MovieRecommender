3c3c70786672d48e4ff3075c3ff9007a2868110c
Joleksiuk <julkaoleksiuk26@gmail.com>
2022-01-08 01:52:23 +0100

    Added Registration

--- MovieRecommender/views.py
+++ MovieRecommender/views.py
@@ -7,6 +7,7 @@ from .forms import RoomForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 rooms = [
     {'id': 1, 'name': 'Lets learn python!'},
@@ -80,12 +81,13 @@ def deleteRoom(request, pk):
 
 
 def loginPage(request):
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
 
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password').lower()
         try:
             user = User.objects.get(username=username)
         except:
@@ -98,10 +100,29 @@ def loginPage(request):
         else:
             messages.error(request, 'Username or password does not exist')
 
-    context = {}
+    context = {'page': page}
     return render(request, 'MovieRecommender/login_register.html', context)
 
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+
+def registerPage(request):
+    page = 'register'
+    form = UserCreationForm()
+    context = {'page': page, 'form': form}
+
+    if request.method == 'POST':
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occured during registration')
+
+    return render(request, 'MovieRecommender/login_register.html', context)
--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('MovieRecommender.urls')),
-    path('room/', include('MovieRecommender.urls'))
+    path('', include('MovieRecommender.urls'))
 ]
