37f90a83fe80191026a646dc779cd154e946bc43
Joleksiuk <julkaoleksiuk26@gmail.com>
2022-01-08 12:58:16 +0100

    Added activity feed and components

--- MovieRecommender/views.py
+++ MovieRecommender/views.py
@@ -26,7 +26,8 @@ def home(request):
     )
     topics = Topic.objects.all()
     room_count = rooms.count()
-    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+    room_messages = Message.objects.filter(Q(room__name__icontains=q))
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'room_messages':room_messages}
     return render(request, 'MovieRecommender/home.html', context)
 
 
